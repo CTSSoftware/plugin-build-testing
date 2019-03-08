4c3c2f3a7258378d3da3d25402217dc39d16a491
Justin Greenwood <justin@cts-software.com>
2019-03-08 09:48:36 -0500

    Added this app to the master channel.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ if (environment.production) {
   enableProdMode();
 }
 
-//keytool -genkey -v -keystore plugin-testing.keystore -alias plugin-testing -keyalg RSA -keysize 2048 -validity 10000
-
+// keytool -genkey -v -keystore plugin-testing.keystore -alias plugin-testing -keyalg RSA -keysize 2048 -validity 10000
+// cordova plugin add cordova-plugin-ionic --save --variable APP_ID="2f648503" --variable CHANNEL_NAME="Master" /
+//                                         --variable UPDATE_METHOD="background"
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
